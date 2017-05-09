e7506da0ead0824ec32e6c431f79b1d939cad063
jstrchn <james.wa.strachan@gmail.com>
2017-05-09 17:18:17 +0200

    Remove junk file

--- scripts/sendData.R
+++
@@ -1,9 +0,0 @@
-<?php
-$to      = 'strachanj@ceu.edu';
-$subject = 'Experiment completed: Participant data attached';
-$message = $json;
-$headers = 'From: noreply@somby.ceu.edu' . "\r\n" .
-'X-Mailer: PHP/' . phpversion();
-
-mail($to, $subject, $message, $headers);
-?>
